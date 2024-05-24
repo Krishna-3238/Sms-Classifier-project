9757adca620d87354d951a9ba52c38e0d5c4b2d8
krishnandan sah <103986066+Krishna-3238@users.noreply.github.com>
2024-05-24 14:24:52 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
 
-nltk.download('punkt')
+def download_nltk_data():
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+    
+    try:
+        nltk.data.find('corpora/stopwords')
+    except LookupError:
+        nltk.download('stopwords')
+
+
+download_nltk_data()
 
 
 def transform_text(text):
